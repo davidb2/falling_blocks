5811503e314da4c2041788050977338ce83f3d0a
David Brewster <brewcrew5498@outlook.com>
2017-08-13 13:27:43 -0400

    added board logic

--- scripts/Board.ts
+++ scripts/Board.ts
@@ -4,10 +4,7 @@ class Board {
     private _playerPosition: [number, number];
     private _board: Block[][];
 
-    constructor(
-        rows: number, 
-        cols: number, 
-        playerPosition: [number, number]) {
+    constructor(rows: number, cols: number, playerPosition: [number, number]) {
         console.assert(
             rows > 0,
             'Number of rows must be a positive integer.'
@@ -16,6 +13,16 @@ class Board {
         console.assert(
             cols > 0, 
             'Number of columns must be a positive integer.'
+        );
+
+        console.assert(
+            0 <= playerPosition[0] && playerPosition[0] < rows,
+            'Player row position must be a non-negative integer less than the number of rows on the board.'
+        );
+
+        console.assert(
+            0 <= playerPosition[1] && playerPosition[1] < cols,
+            'Player column position must be a non-negative integer less than the number of columns on the board.'
         );
 
         this._rows = rows;
@@ -33,7 +40,45 @@ class Board {
                 }
             }
         }
-
     }
 
+    get rows(): number { return this._rows; }
+
+    get cols(): number { return this._cols; }
+
+    get playerPosition(): [number, number] { return this._playerPosition; }
+
+    get board(): Block[][] { return this._board; }
+
+    public movePlayerLeft() {
+        if (this._playerPosition[1] > 0) {
+            this._board[this._board.length - 1][this._playerPosition[1]] = new Blank();
+            if (this._board[this._board.length - 1][this._playerPosition[1] - 1] instanceof Obstacle) {
+                // Player has crashed into an obstacle.
+                this._playerPosition = null;
+            } else if (this._board[this._board.length - 1][this._playerPosition[1] - 1] instanceof Blank) {
+                // Player has made a safe move.
+                this._board[this._board.length - 1][this._playerPosition[1] - 1] = new Player();
+                this._playerPosition = [this._playerPosition[0], this._playerPosition[1] - 1];
+            } else {
+                console.error('Impossible state: Player has run into another player.');
+            }
+        }
+    }
+
+    public movePlayerRight() {
+        if (this._playerPosition[1] < this._cols - 1) {
+            this._board[this._board.length - 1][this._playerPosition[1]] = new Blank();
+            if (this._board[this._board.length - 1][this._playerPosition[1] + 1] instanceof Obstacle) {
+                // Player has crashed into an obstacle.
+                this._playerPosition = null;
+            } else if (this._board[this._board.length - 1][this._playerPosition[1] + 1] instanceof Blank) {
+                // Player has made a safe move.
+                this._board[this._board.length - 1][this._playerPosition[1] + 1] = new Player();
+                this._playerPosition = [this._playerPosition[0], this._playerPosition[1] + 1];
+            } else {
+                console.error('Impossible state: Player has run into another player.');
+            }
+        }
+    }
 }
